35af793239970477ebd95d67a6a2b967bbae30e0
markberencsi <markberencsi@gmail.com>
2022-05-31 14:49:23 +0200

    Spring project created

---
+++ src/test/java/com/codecool/proyecteGrande/ProyecteGrandeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.proyecteGrande;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyecteGrandeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
