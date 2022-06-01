173fada6bcd5a6d5f5570348903b92bcd7c186cd
wdattila <wdattila@gmail.com>
2022-06-01 14:12:59 +0200

    ADD: HouseDao interface basic methods

--- src/main/java/com/codecool/proyecteGrande/dao/HouseDao.java
+++ src/main/java/com/codecool/proyecteGrande/dao/HouseDao.java
@@ -1,4 +1,13 @@
 package com.codecool.proyecteGrande.dao;
 
+import com.codecool.proyecteGrande.model.House;
+
+import java.util.List;
+
 public interface HouseDao {
+    void add(House house);
+    House find(int id);
+    void remove(int id);
+
+    List<House> getAll();
 }
