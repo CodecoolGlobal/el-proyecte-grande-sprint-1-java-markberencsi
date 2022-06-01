339de4473a3aea1733fb3079383f6f42f517ad4d
wdattila <wdattila@gmail.com>
2022-06-01 14:20:30 +0200

    ADD: HouseService getAll method implementation

--- src/main/java/com/codecool/proyecteGrande/service/HouseService.java
+++ src/main/java/com/codecool/proyecteGrande/service/HouseService.java
@@ -1,11 +1,18 @@
 package com.codecool.proyecteGrande.service;
 
 import com.codecool.proyecteGrande.dao.HouseDao;
+import com.codecool.proyecteGrande.model.House;
+
+import java.util.List;
 
 public class HouseService {
     private HouseDao houseDao;
 
     public HouseService(HouseDao houseDao) {
         this.houseDao = houseDao;
+    }
+
+    public List<House> getAll(){
+        return houseDao.getAll();
     }
 }
