02b1f901979000a6843ce327b915ca2618b4208c
wdattila <wdattila@gmail.com>
2022-06-01 14:14:27 +0200

    ADD: HouseDaoMem empty implementations of the HouseDao interface

--- src/main/java/com/codecool/proyecteGrande/dao/implementation/HouseDaoMem.java
+++ src/main/java/com/codecool/proyecteGrande/dao/implementation/HouseDaoMem.java
@@ -1,4 +1,43 @@
 package com.codecool.proyecteGrande.dao.implementation;
 
-public class HouseDaoMem {
+import com.codecool.proyecteGrande.dao.HouseDao;
+import com.codecool.proyecteGrande.model.House;
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+public class HouseDaoMem implements HouseDao {
+    private List<House> houses;
+
+    public HouseDaoMem() {
+        houses = new ArrayList<>();
+    }
+
+    @Override
+    public void add(House house) {
+
+    }
+
+    @Override
+    public House find(int id) {
+        return null;
+    }
+
+    @Override
+    public void remove(int id) {
+
+    }
+
+    @Override
+    public List<House> getAll() {
+        return null;
+    }
 }
