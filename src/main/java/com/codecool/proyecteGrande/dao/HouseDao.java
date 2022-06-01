b4d5b7b54997dd5c66a93cac9959badac2c32c4c
wdattila <wdattila@gmail.com>
2022-06-01 14:27:08 +0200

    ADD: Adding and autowiring HouseDao and HouseService beans

--- src/main/java/com/codecool/proyecteGrande/dao/HouseDao.java
+++ src/main/java/com/codecool/proyecteGrande/dao/HouseDao.java
@@ -1,9 +1,11 @@
 package com.codecool.proyecteGrande.dao;
 
 import com.codecool.proyecteGrande.model.House;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+@Component
 public interface HouseDao {
     void add(House house);
     House find(int id);
--- src/main/java/com/codecool/proyecteGrande/service/HouseService.java
+++ src/main/java/com/codecool/proyecteGrande/service/HouseService.java
@@ -2,10 +2,14 @@ package com.codecool.proyecteGrande.service;
 
 import com.codecool.proyecteGrande.dao.HouseDao;
 import com.codecool.proyecteGrande.model.House;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+@Component
 public class HouseService {
+    @Autowired
     private HouseDao houseDao;
 
     public HouseService(HouseDao houseDao) {
