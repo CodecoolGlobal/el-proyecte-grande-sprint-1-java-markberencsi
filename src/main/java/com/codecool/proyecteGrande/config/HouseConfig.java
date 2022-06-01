6366c1f7d46e90a483785f84756959aa04e2f6a2
wdattila <wdattila@gmail.com>
2022-06-01 14:29:30 +0200

    ADD: HouseConfig for configuring beans related to House model

---
+++ src/main/java/com/codecool/proyecteGrande/config/HouseConfig.java
@@ -0,0 +1,26 @@
+package com.codecool.proyecteGrande.config;
+
+import com.codecool.proyecteGrande.controller.HouseController;
+import com.codecool.proyecteGrande.dao.HouseDao;
+import com.codecool.proyecteGrande.dao.implementation.HouseDaoMem;
+import com.codecool.proyecteGrande.service.HouseService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HouseConfig {
+    @Bean
+    public HouseDao getHouseDao(){
+        return new HouseDaoMem();
+    }
+
+    @Bean
+    public HouseService getHouseService(){
+        return new HouseService(getHouseDao());
+    }
+
+    @Bean
+    public HouseController getHouseController(){
+        return new HouseController(getHouseService());
+    }
+}
