3a8ea5229d17206e29017dcb4c27c4824dcb41c6
markberencsi <markberencsi@gmail.com>
2022-06-01 14:39:02 +0200

    ADD:Teacher model and Dao classes implemented

--- src/main/java/com/codecool/proyecteGrande/dao/TeacherDao.java
+++ src/main/java/com/codecool/proyecteGrande/dao/TeacherDao.java
@@ -1,4 +1,19 @@
 package com.codecool.proyecteGrande.dao;
 
+import com.codecool.proyecteGrande.model.Subject;
+import com.codecool.proyecteGrande.model.Teacher;
+
+import java.util.List;
+
 public interface TeacherDao {
+    void addTeacher(Teacher teacher);
+
+    void addTeacher(String firstName, String lastName, String email, String password, Subject taughtSubject);
+
+    void removeTeacherById(int id);
+
+    void updateTeacher(int id, Subject newTaughtSubject);
+
+    List<Teacher> getAll();
+    Teacher getTeacherById(int id);
 }
--- src/main/java/com/codecool/proyecteGrande/model/Teacher.java
+++ src/main/java/com/codecool/proyecteGrande/model/Teacher.java
@@ -1,4 +1,52 @@
 package com.codecool.proyecteGrande.model;
 
 public class Teacher {
+
+    private int id;
+    private final String firstName;
+    private final String lastName;
+    private String email;
+    private String password;
+
+    private Subject taughtSubject;
+
+    public Teacher(String firstName, String lastName, String email, String password, Subject taughtSubject) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.taughtSubject = taughtSubject;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public Subject getTaughtSubject() {
+        return taughtSubject;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setTaughtSubject(Subject taughtSubject) {
+        this.taughtSubject = taughtSubject;
+    }
 }
