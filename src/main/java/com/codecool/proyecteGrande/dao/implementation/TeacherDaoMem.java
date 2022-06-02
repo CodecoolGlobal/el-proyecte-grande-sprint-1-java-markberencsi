59f5b537666d11c3816b84918b94073730763848
markberencsi <markberencsi@gmail.com>
2022-06-02 10:14:34 +0200

    FIX: Teacher entries now get proper id-s when added to the memory storage

--- src/main/java/com/codecool/proyecteGrande/dao/implementation/TeacherDaoMem.java
+++ src/main/java/com/codecool/proyecteGrande/dao/implementation/TeacherDaoMem.java
@@ -10,14 +10,20 @@ import java.util.List;
 public class TeacherDaoMem implements TeacherDao {
 
     private final List<Teacher> teachers = new ArrayList<>();
+    private int quantityCounter = 0;
 
     @Override
     public void addTeacher(Teacher teacher){
+        quantityCounter++;
+        teacher.setId(quantityCounter);
         teachers.add(teacher);
     }
     @Override
     public void addTeacher(String firstName, String lastName, String email, String password, Subject taughtSubject){
-        teachers.add(new Teacher(firstName,lastName,email,password,taughtSubject));
+        quantityCounter++;
+        Teacher newTeacher = new Teacher(firstName,lastName,email,password,taughtSubject);
+        newTeacher.setId(quantityCounter);
+        teachers.add(newTeacher);
     }
     @Override
     public void removeTeacherById(int id){
