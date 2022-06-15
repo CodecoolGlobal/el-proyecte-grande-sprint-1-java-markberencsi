cac2b34f484db9947f52dc07c235fa07acaac437
wdattila <wdattila@gmail.com>
2022-06-15 13:25:39 +0200

    ADD: react routes

--- client/harry-pointer/src/App.js
+++ client/harry-pointer/src/App.js
@@ -2,18 +2,28 @@ import './App.css';
 import Navbar from './Navbar';
 import{ Heading } from './Navbar';
 import background from './hogwarts.jpg'
+import Houses from './houses/Houses'
+import { fetchGetAll } from './houses/api/houseApi';
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
+
 
 function App() {
+  fetchGetAll();
   return (
     <div className="App" style={{ 
-      backgroundImage: `url(${background})`, 
-      backgroundSize:"contain", 
-      backgroundRepeat: "no-repeat",
-      width: "auto",
-      height: "850px"
-  }}>
-      <Navbar />
-      <Heading />
+        backgroundImage: `url(${background})`, 
+        backgroundSize:"contain", 
+        backgroundRepeat: "no-repeat",
+        width: "auto",
+        height: "850px"
+    }}>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route exact path='/' element={<Heading />} />
+          <Route path='/houses' element={<Houses />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
