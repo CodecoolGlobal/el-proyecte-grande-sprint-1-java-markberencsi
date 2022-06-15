47c53bc0f74f18de11b22bf950721f42c5442c87
markberencsi <markberencsi@gmail.com>
2022-06-15 14:17:04 +0200

    ADD: new better design created for the teacher component

--- client/harry-pointer/src/Teacher.js
+++ client/harry-pointer/src/Teacher.js
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import api from "./api";
+import "./teacher.css";
 
 
 
@@ -16,4 +17,48 @@ export const TeacherList = () => {
             <> {teachers.map(teacher => <li style={{color: "white"}}  key={teacher.id}>{teacher.firstName} {teacher.lastName} is our {teacher.taughtSubject} professor, learn this subject here: <a href="#">Apply</a></li>)}</>
         </ol>
     )
+};
+
+export const ProjectCardsFlip = () => {
+
+    const [data, setData] = useState([])
+    useEffect(() => {
+        api.get("/api/teachers/all").then((response) => {
+            setData(response.data)
+        });
+    }, []);
+    const teachers = data
+
+    return (
+        <>
+            <div className="teacherTitle"><h1>Our Teachers</h1></div>
+            {teachers.map(teacher =>
+        <div key={teacher.id}>
+            <div className="wrapper">
+                <div className="cols">
+                    <div className="col" onTouchStart="this.classList.toggle('hover');">
+                        <div className="container">
+                            <div className="front">
+                                <div className="inner">
+                                    <p>{teacher.firstName} {teacher.lastName}</p>
+                                    <span>{teacher.taughtSubject}</span>
+                                </div>
+                            </div>
+                            <div className="back">
+                                <div className="inner">
+                                    <span>
+                    Click here to apply for this subject!{" "}
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )}
+        </>
+
+
+    );
 };
