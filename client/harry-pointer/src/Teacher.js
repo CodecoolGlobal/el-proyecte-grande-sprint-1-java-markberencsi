f795d62569082cb1a986c71c7963d7e0b9357fa3
markberencsi <markberencsi@gmail.com>
2022-06-15 11:13:18 +0200

    ADD: teachers can be displayed in a list form

---
+++ client/harry-pointer/src/Teacher.js
@@ -0,0 +1,18 @@
+import React, {useEffect, useState} from 'react';
+
+
+
+export const TeacherList = () => {
+    const [data, setData] = useState([])
+    useEffect(() => {
+        api.get("/api/teachers/all").then((response) => {
+            setData(response.data)
+        });
+    }, []);
+    const teachers = data
+    return (
+        <ol type="1">
+            <> {teachers.map(teacher => <li style={{color: "white"}}  key={teacher.id}>{teacher.firstName} {teacher.lastName} is our {teacher.taughtSubject} professor, learn this subject here: <a href="#">Apply</a></li>)}</>
+        </ol>
+    )
+};
