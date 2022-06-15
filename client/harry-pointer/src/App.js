13189b9dad33404cce719e035f4eef6a34f5ede6
kovacsadam199 <kovacsadam199@gmail.com>
2022-06-15 10:53:44 +0200

    ADD: students can be listed with api call in an ordered list

--- client/harry-pointer/src/App.js
+++ client/harry-pointer/src/App.js
@@ -3,19 +3,20 @@ import Navbar from './Navbar';
 import{ Heading } from './Navbar';
 import background from './hogwarts.jpg'
 import Students from "./Students";
+import api from "./api";
 
 function App() {
-  return (
-    <div className="App" style={{ 
-      backgroundImage: `url(${background})`, 
-      backgroundSize:"contain", 
+    return (
+    <div className="App" style={{
+      backgroundImage: `url(${background})`,
+      backgroundSize:"contain",
       backgroundRepeat: "no-repeat",
       width: "auto",
       height: "850px"
   }}>
       <Navbar />
       <Heading />
-        {/*<Students  students={getStudents()}/>*/}
+        <Students  />
     </div>
   );
 }
