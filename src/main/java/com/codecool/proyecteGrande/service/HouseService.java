519eb87f39792964117214699c79f31e554a3751
wdattila <wdattila@gmail.com>
2022-06-28 14:28:00 +0200

    REFACTOR: HouseService service annotation

--- src/main/java/com/codecool/proyecteGrande/service/HouseService.java
+++ src/main/java/com/codecool/proyecteGrande/service/HouseService.java
@@ -1,22 +1,21 @@
 package com.codecool.proyecteGrande.service;
 
 import com.codecool.proyecteGrande.dao.HouseDao;
+import com.codecool.proyecteGrande.dao.HouseRepository;
 import com.codecool.proyecteGrande.model.House;
+import com.codecool.proyecteGrande.model.HouseEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Component
+@Service
 public class HouseService {
     @Autowired
-    private HouseDao houseDao;
+    private HouseRepository houseRepository;
 
-    public HouseService(HouseDao houseDao) {
-        this.houseDao = houseDao;
-    }
-
-    public List<House> getAll(){
-        return houseDao.getAll();
+    public List<HouseEntity> getAll(){
+        return houseRepository.findAll();
     }
 }
