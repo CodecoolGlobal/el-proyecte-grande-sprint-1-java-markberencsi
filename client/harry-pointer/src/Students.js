319fb55be8f8cbd4b7b404108462d73b5d2b2b5f
kovacsadam199 <kovacsadam199@gmail.com>
2022-06-29 14:25:16 +0200

    ADD: can filter search students

--- client/harry-pointer/src/Students.js
+++ client/harry-pointer/src/Students.js
@@ -2,8 +2,18 @@ import api from "./api";
 import {useEffect, useState} from "react";
 
 
+
 const Students =  () => {
+
     const [data, setData] = useState([])
+    const [filteredData,setFilteredData] = useState([])
+    const searchItems = (searchValue) => {
+        setFilteredData(data.filter((item) => {
+            return Object.values(item).join('').toLowerCase().includes(searchValue.toLowerCase())
+        }))
+        console.log("search input: "+searchValue )
+    }
+
     useEffect(() => {
         api.get("/api/student/all").then((response) => {
             setData(response.data)
@@ -11,15 +21,23 @@ const Students =  () => {
 
         });
     }, []);
-    const students = data
+    const students = filteredData.length > 0 ? filteredData  : data
+    console.log(filteredData.length)
+    console.log(students)
     return (
 
         <div className='p-5 text-center bg-dark' style={{
             margin: "40px 50px 0",
             borderRadius: "10px"}}>
+
             <h1>Our students</h1>
+            <input style={{marginBottom:20}}
+                placeholder='Search...'
+                onChange={(e) => searchItems(e.target.value)
+                }
+            />
             <ol type="1">
-                <> {students.map(student => <li style={{color: "whitesmoke"}}  key={student.id}>{student.firstName} {student.lastName} {student.email}</li>)}</>
+                 <> {students.map(student => <li style={{color: "whitesmoke"}}  key={student.id}>{student.firstName} {student.lastName} {student.email}</li>)}</>
             </ol>
         </div>
 
