596621e722c6fe63654a18d3aa547369420e0440
wdattila <wdattila@gmail.com>
2022-06-30 17:05:10 +0200

    ADD: findbyname to HouseRepository

--- src/main/java/com/codecool/proyecteGrande/dao/HouseRepository.java
+++ src/main/java/com/codecool/proyecteGrande/dao/HouseRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface HouseRepository extends JpaRepository<HouseEntity, Long> {
+    HouseEntity findByName(String name);
 }
