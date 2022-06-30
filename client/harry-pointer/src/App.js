f980ef35a8454f22078291a5ee221ee6f08f75d7
wdattila <wdattila@gmail.com>
2022-06-30 09:28:45 +0200

    ADD: leaderboard route path

--- client/harry-pointer/src/App.js
+++ client/harry-pointer/src/App.js
@@ -1,14 +1,13 @@
 import './App.css';
 import Navbar from './Navbar';
 import{ Heading } from './Navbar';
-import background from './hogwarts.jpg'
 import Houses from './houses/Houses'
 import { fetchGetAll } from './houses/api/houseApi';
 import { Route, BrowserRouter, Routes } from 'react-router-dom';
-import {ProjectCardsFlip, TeacherList} from "./Teacher";
+import {ProjectCardsFlip} from "./Teacher";
 
 import Students from "./Students";
-import api from "./api";
+import Leaderboard from './houses/Leaderboard';
 
 function App() {
   fetchGetAll();
@@ -20,7 +19,8 @@ function App() {
           <Route exact path='/' element={<Heading />} />
           <Route path='/houses' element={<Houses />} />
           <Route path='/teachers' element={<ProjectCardsFlip/>}/>
-            <Route path='/students' element={<Students />} />
+          <Route path='/students' element={<Students />} />
+          <Route path='/leaderboard' element={<Leaderboard />} />
         </Routes>
       </BrowserRouter>
     </div>
