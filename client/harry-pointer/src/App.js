4333bb5c3770ccaa96abfe6ffd4a23dd8b2abbb4
wdattila <wdattila@gmail.com>
2022-06-30 17:07:04 +0200

    ADD: sub routes in the houses

--- client/harry-pointer/src/App.js
+++ client/harry-pointer/src/App.js
@@ -3,11 +3,12 @@ import Navbar from './Navbar';
 import{ Heading } from './Navbar';
 import Houses from './houses/Houses'
 import { fetchGetAll } from './houses/api/houseApi';
-import { Route, BrowserRouter, Routes } from 'react-router-dom';
+import { Route, BrowserRouter, Routes, Outlet } from 'react-router-dom';
 import {ProjectCardsFlip} from "./Teacher";
 
 import Students from "./Students";
 import Leaderboard from './houses/Leaderboard';
+import HouseDetail from './houses/HouseDetail';
 
 function App() {
   fetchGetAll();
@@ -17,7 +18,10 @@ function App() {
         <Navbar />
         <Routes>
           <Route exact path='/' element={<Heading />} />
-          <Route path='/houses' element={<Houses />} />
+          <Route path='/houses' element={<Outlet />}>
+            <Route index element={<Houses/>}/>
+            <Route path=':name' element={<HouseDetail />}/>
+          </Route>
           <Route path='/teachers' element={<ProjectCardsFlip/>}/>
           <Route path='/students' element={<Students />} />
           <Route path='/leaderboard' element={<Leaderboard />} />
