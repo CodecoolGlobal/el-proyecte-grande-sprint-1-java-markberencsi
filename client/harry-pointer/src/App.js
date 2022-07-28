bab8b856e81b18223f810bcbc1ea1874dbfc4c2a
wdattila <wdattila@gmail.com>
2022-07-28 16:51:51 +0200

    ADD: auth context, and requirement

--- client/harry-pointer/src/App.js
+++ client/harry-pointer/src/App.js
@@ -10,25 +10,31 @@ import {LoginForm} from "./Login";
 import Students from "./Students";
 import Leaderboard from './houses/Leaderboard';
 import HouseDetail from './houses/HouseDetail';
+import AuthProvider from './authentication/AuthProvider';
+import RequiredAuth from './authentication/RequiredAuth';
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route exact path='/' element={<Heading />} />
-          <Route path='/houses' element={<Outlet />}>
-            <Route index element={<Houses/>}/>
-            <Route path=':name' element={<HouseDetail />}/>
-          </Route>
-          <Route path='/teachers' element={<ProjectCardsFlip/>}/>
-          <Route path='/students' element={<Students />} />
-          <Route path='/leaderboard' element={<Leaderboard />} />
-          <Route path='/signup' element={<SignupForm />} />
-          <Route path='/login' element={<LoginForm />} />
-        </Routes>
-      </BrowserRouter>
+      <AuthProvider>
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route exact path='/' element={<Heading />} />
+            <Route path='/' element={<RequiredAuth />} >
+              <Route path='/houses' element={<Outlet />}>
+                <Route index element={<Houses/>}/>
+                <Route path=':name' element={<HouseDetail />}/>
+              </Route>
+              <Route path='/teachers' element={<ProjectCardsFlip/>}/>
+              <Route path='/students' element={<Students />} />
+              <Route path='/leaderboard' element={<Leaderboard />} />
+            </Route>
+            <Route path='/signup' element={<SignupForm />} />
+            <Route path='/login' element={<LoginForm />} />
+          </Routes>
+        </BrowserRouter>
+      </AuthProvider>
     </div>
   );
 }
