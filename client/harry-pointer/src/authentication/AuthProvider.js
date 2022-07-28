b0d6d0f46605aed8309908202ce59b12a909733a
wdattila <wdattila@gmail.com>
2022-07-28 16:49:53 +0200

    ADD: authentication context

---
+++ client/harry-pointer/src/authentication/AuthProvider.js
@@ -0,0 +1,39 @@
+import { createContext, useState } from "react";
+import api from "../api";
+
+export const AuthContext = createContext(null);
+const getToken = async (emailOrUsername, password) => {
+    try {
+        const response = await api.post("/api/login", {
+            email: emailOrUsername,
+            password: password
+        });
+        return response.data;
+    } catch (error) {
+        console.log(error.response);
+    }
+}
+
+const AuthProvider = ({children}) => {
+    const [accesToken, setAccessToken] = useState(localStorage.getItem('accessToken'));
+
+    const login = async (username, password, callback) => {
+        const token = await getToken(username, password);
+        setAccessToken(token);
+        localStorage.setItem("accessToken", token);
+        callback();
+    }
+
+    const logout = () => {
+        localStorage.removeItem("accessToken");
+        setAccessToken(null);
+    }
+
+    const values = {accesToken, login, logout};
+
+    return <AuthContext.Provider value={values}>
+        {children}
+    </AuthContext.Provider>
+}
+
+export default AuthProvider;
