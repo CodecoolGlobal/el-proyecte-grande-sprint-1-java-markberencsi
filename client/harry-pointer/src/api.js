ac95aff47fb6d9428657e6bc5a02000e733b2e66
wdattila <wdattila@gmail.com>
2022-07-28 12:28:21 +0200

    ADD: axios intercept response, request

--- client/harry-pointer/src/api.js
+++ client/harry-pointer/src/api.js
@@ -4,4 +4,30 @@ const api = axios.create({
     baseURL: process.env.REACT_APP_BASE_URL
 })
 
+api.interceptors.request.use(
+    request => {
+        const accesToken = localStorage.getItem('accesToken');
+        request.headers["Authorization"] = "Bearer " + accesToken;
+        return request;
+    },
+    error => {
+        return Promise.reject(error);
+    }
+)
+
+api.interceptors.response.use(
+    (response) => {
+        console.log(response.status);
+        return response;
+    },
+    error => {
+        console.log(error);
+        const response = error.response;
+        if(response.status === 401) {
+            window.location.href = "/login";
+        }
+        console.log(error);
+        return Promise.reject(error);
+    }
+)
 export default api;
