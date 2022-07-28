ceb3c79f7b25f29e84ee7578e2393609b06ac5ad
markberencsi <markberencsi@gmail.com>
2022-07-28 13:09:51 +0200

    REFACTOR: Userfront implementetion removed

--- client/harry-pointer/src/Signup.js
+++ client/harry-pointer/src/Signup.js
@@ -1,120 +1,89 @@
-// Initialize Userfront Core JS
-import Userfront from "@userfront/core";
-import React from "react";
+import React, {useState} from "react";
 import style from "./signup.module.css";
 
-Userfront.init("demo1234");
+function SignupForm() {
+    const [first_name, setFirstName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [last_name, setLastName] = useState("");
+    const [message, setMessage] = useState("");
 
-// Define the Signup form component
-class SignupForm extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            email: "",
-            accountName: "",
-            password: "",
-            passwordVerify: "",
-            alertMessage: ""
-        };
-
-        this.handleInputChange = this.handleInputChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.setAlertMessage = this.setAlertMessage.bind(this);
-    }
-
-    // Whenever an input changes value, change the corresponding state variable
-    handleInputChange(event) {
-        event.preventDefault();
-        const target = event.target;
-        this.setState({
-            [target.name]: target.value
-        });
-    }
-
-    // Handle the form submission by calling Userfront.signup()
-    handleSubmit(event) {
-        event.preventDefault();
-        // Reset the alert to empty
-        this.setAlertMessage();
-        // Verify that the passwords match
-        if (this.state.password !== this.state.passwordVerify) {
-            return this.setAlertMessage('Passwords must match');
-        }
-        // Call Userfront.signup()
-        Userfront.signup({
-            method: "password",
-            email: this.state.email,
-            password: this.state.password,
-            data: {
-                accountName: this.state.accountName
+    let handleSubmit = async (e) => {
+        console.log(first_name);
+        console.log(email);
+        console.log(password);
+        e.preventDefault();
+        try {
+            let res = await fetch("http://localhost:8080/api/signup", {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json'
+                    // 'Content-Type': 'application/x-www-form-urlencoded',
+                },
+                body: JSON.stringify({
+                    "first_name": first_name,
+                    "last_name": last_name,
+                    "email": email,
+                    "password": password,
+                }),
+            });
+            if (res.status === 200) {
+                setFirstName("");
+                setEmail("");
+                setLastName("");
+                setPassword("");
+                console.log("OK")
+                setMessage("User created successfully");
+            } else {
+                console.log("NOTOK")
+                setMessage("This e-mail is not available");
             }
-        }).catch((error) => {
-            this.setAlertMessage(error.message);
-        });
-    }
+        } catch (err) {
+            console.log(err);
+        }
+    };
 
-    setAlertMessage(message) {
-        this.setState({ alertMessage: message });
-    }
+    return (
+        <div className={style.signup_container}>
+            <form onSubmit={handleSubmit}>
+                <label>First name
+                <input
+                    type="text"
+                    value={first_name}
+                    placeholder="First name"
+                    onChange={(e) => setFirstName(e.target.value)}
+                />
+                </label>
+                <label>Last name
+                    <input
+                        type="text"
+                        value={last_name}
+                        placeholder="First name"
+                        onChange={(e) => setLastName(e.target.value)}
+                    />
+                </label>
+                <label> Your Email
+                <input
+                    type="text"
+                    value={email}
+                    placeholder="Email"
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+                </label>
+                <label> Password
+                <input
+                    type="password"
+                    value={password}
+                    placeholder="Password"
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                </label>
+                <button className={style.signup_button} type="submit">Create</button>
 
-    render() {
-        return (
-            <div className={style.signup_container}>
-                <Alert message={this.state.alertMessage} />
-                <form onSubmit={this.handleSubmit}>
-                    <label>
-                        Email address
-                        <input
-                            name="email"
-                            type="email"
-                            value={this.state.email}
-                            onChange={this.handleInputChange}
-                        />
-                    </label>
-                    <label>
-                        Username
-                        <input
-                            name="first_name"
-                            type="text"
-                            value={this.state.accountName}
-                            onChange={this.handleInputChange}
-                        />
-                    </label>
-                    <label>
-                        Password
-                        <input
-                            name="password"
-                            type="password"
-                            value={this.state.password}
-                            onChange={this.handleInputChange}
-                        />
-                    </label>
-                    <label>
-                        Verify password
-                        <input
-                            name="passwordVerify"
-                            type="password"
-                            value={this.state.passwordVerify}
-                            onChange={this.handleInputChange}
-                        />
-                    </label>
-                    <button className={style.signup_button} type="submit">Sign up</button>
-                </form>
-            </div>
-        );
-    }
+                <div className="message">{message ? <p>{message}</p> : null}</div>
+            </form>
+        </div>
+    );
 }
-
-// Define the alert component
-class Alert extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-    render() {
-        if (!this.props.message) return "";
-        return <div id="alert">{this.props.message}</div>;
-    }
-}
-
 
 export { SignupForm };
